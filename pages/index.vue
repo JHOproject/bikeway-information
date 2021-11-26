1b2202adb7775feb1d34133d1c94c8d07be04c6a
jessie <jessie@sjis.com.tw>
2021-11-26 18:32:14 +0800

    [add] index 首頁封面

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <Tutorial/>
+  <div class="bg-landing-image bg-cover w-screen h-screen">
+    <img
+      src="@/assets/icon/logo_4x.png"
+      alt="logo"
+      class="absolute top-1/4 left-32"
+    />
+    <img
+      src="@/assets/icon/logo_text_4x.png"
+      alt="logoText"
+      class="absolute top-1/2 left-32"
+    />
+  </div>
 </template>
 
 <script lang="ts">
